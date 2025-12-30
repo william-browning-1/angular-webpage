2e805e4452b33483128a3a6cb8aa9cbe0c17a946
unknown <wdbrwnng@memphis.edu>
2025-12-30 15:25:24 -0600

    Changing Branches/Currently-Broken

--- personal-website/src/app/core/header/header.component.ts
+++ personal-website/src/app/core/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [NavComponent],
+  imports: [NavComponent, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
--- personal-website/src/app/core/header/nav/nav.component.ts
+++ personal-website/src/app/core/header/nav/nav.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  constructor(private router: Router) {}
 
+  goTo(path: string) {
+    this.router.navigateByUrl(path);
+  }
+
+  toggleMenu(open: boolean) {
+    document.body.classList.toggle('menu-open', !!open);
+  }
 }
