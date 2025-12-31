53597d7c6373b48cc821072401b27c6a351c1095
unknown <wdbrwnng@memphis.edu>
2025-12-31 08:43:32 -0600

    Nav Sidebar and Setup Main Components

--- personal-website/src/app/app.component.ts
+++ personal-website/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/header/header.component';
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ personal-website/src/app/nav/nav.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+
+  menuItems = [
+    { label: 'Home', icon: 'fa-home', link: '/' },
+    { label: 'Contact', icon: 'fa-comments', link: '/contact' },
+    { label: 'Education', icon: 'fa-book', link: '/education' },
+    { label: 'Docs', icon: 'fa-info', link: '/docs' }
+  ];
+
+  // logoutItem = {
+  //   label: 'Logout',
+  //   icon: 'fa-power-off',
+  //   link: '/logout'
+  // };
+}
